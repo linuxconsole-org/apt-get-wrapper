8d1aee4939fbd528aaabb6b78b147a721c593ed6
Yann Le Doaré <yann@linuxconsole.org>
2026-09-07 16:40:00 +0200

    Rewrite extract.sh

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # A script to install deb packages on $HOME
 
 > Used from Your Distrofromscratch / LinuxConsole project
+> Debian is now using a javascript control access to download files, we have to exatract links first
 
-> Unbuntu repositories since Debian is using a javascript control access to download files
+# Find uris
+```
+apt-get install --print-uris flatpak | grep flatpak_
+```
 
-grep flatpak_1.14.6-1ubuntu0.1 /var/lib/apt/lists/*
+```
+ grep flatpak_1.14.6-1ubuntu0.1 /var/lib/apt/lists/*
+```
--- extract.sh
+++ extract.sh
@@ -1,14 +1,28 @@
 # FIXME : this is realy slow !
 # To rewrite in python 
-for list in ftp.fr.debian.org_debian_dists_trixie_main_binary-amd64_Packages # archive.ubuntu.com_ubuntu_dists_noble_main_binary-amd64_Packages
+
+if [ -e /var/lib/apt/lists/_etc_apt_mirrors_debian.list_dists_bookworm_main_binary-amd64_Packages ]
+then
+	LIST=/var/lib/apt/lists/_etc_apt_mirrors_debian.list_dists_bookworm_main_binary-amd64_Packages
+	URL=http://archive.ubuntu.com/ubuntu
+fi
+
+if [ -e /var/lib/apt/lists/security.ubuntu.com_ubuntu_dists_noble-security_universe_binary-amd64_Packages ]
+then
+	LIST=/var/lib/apt/lists/security.ubuntu.com_ubuntu_dists_noble-security_universe_binary-amd64_Packages
+	URL=http://archive.ubuntu.com/ubuntu
+fi
+
+if [ -e /var/lib/apt/lists/_etc_apt_mirrors_debian.list_dists_bookworm_main_binary-amd64_Packages ]
+then
+	LIST=/var/lib/apt/lists/_etc_apt_mirrors_debian.list_dists_bookworm_main_binary-amd64_Packages
+	URL=http://ftp.fr.debian.org/debian
+fi
+
+[ "$LIST" = "" ] && echo "No list found" && exit 1
+
+cat $LIST | while read line
 do
-  [ ! -e /var/lib/apt/lists/$list ] && continue
-  grep -e 'Package:' -e 'Filename:' /var/lib/apt/lists/$list > packages.txt
-  cat packages.txt | while read line
-  do
-    echo $line | grep Package >/dev/null && PACKAGE=$(echo $line | cut -d':' -f2)
-    # echo $line | grep Filename >/dev/null && FILENAME=$(echo $line | tr -d ' ' | cut -d':' -f2) && echo $PACKAGE:http://archive.ubuntu.com/ubuntu/$FILENAME # && break
-    echo $line | grep Filename >/dev/null && FILENAME=$(echo $line | tr -d ' ' | cut -d':' -f2) && echo $PACKAGE:http://ftp.fr.debian.org/debian/$FILENAME # && break
-  done
-  break
+  echo $line | grep Package >/dev/null && PACKAGE=$(echo $line | cut -d':' -f2)
+  echo $line | grep Filename >/dev/null && FILENAME=$(echo $line | tr -d ' ' | cut -d':' -f2) && echo $PACKAGE:$URL/$FILENAME # && break
 done
